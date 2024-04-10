0cd9cfafa76625a051319108bf01b71be92fbac8
RiccardoOrlandi <166627174+RiccardoOrlandi@users.noreply.github.com>
2024-04-10 23:49:14 +0200

    Add files via upload

--- gps_to_odom.cpp
+++ gps_to_odom.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
+
 
 void pubCallback( const std_msgs::String::ConstPtr& msg){
 
@@ -13,9 +13,8 @@ int main(int argc , char** argv){
     ros::init(argc, argv, "gps_to_odom");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("/fix", 10, pubCallback);
+    ros::Subscriber sub1 = n.subscribe("/fix", 10, pubCallback);
     ros::spin();
 
     return 0;
 }
-
