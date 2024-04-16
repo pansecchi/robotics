efa996611129ff96220f34cdbf318bf28377e8a8
pansecchi <140237901+pansecchi@users.noreply.github.com>
2024-04-16 17:19:44 +0200

    Add files via upload

--- gps_to_odom.cpp
+++ gps_to_odom.cpp
@@ -1,5 +1,8 @@
 #include "ros/ros.h"
 #include "sensor_msgs/NavSatFix.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Quaternion.h"
 #include "math.h"
 
 struct GPSPoint {
@@ -77,6 +80,12 @@ ENUPoint convertECEFToENU(const ECEFPoint& ecef) {
     return enu;
 }
 
+double calculateDirection(double x1, double y1, double x2, double y2) {
+    // Calcola l'angolo in radianti tra due punti (x1, y1) e (x2, y2)
+    return atan2(y2 - y1, x2 - x1);
+}
+
+ros::Publisher odom_pub;
 
 void Callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
 
@@ -90,10 +99,38 @@ void Callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
 
     ENUPoint enu = convertECEFToENU(ecef);
 
+    /*ROS_INFO("e: %f n: %f u:%f h: %f", enu.e , enu.n , enu.u , heading);*/
     
+    double direction = calculateDirection(prev_enu.e, prev_enu.n, enu.e, enu.n);
 
-    heading = atan( ( enu.n - prev_enu.n ) / ( enu.e - prev_enu.e ) );
-    ROS_INFO("e: %f n: %f u:%f h: %f", enu.e , enu.n , enu.u , heading);
+    nav_msgs::Odometry odom;
+    odom.header.stamp = ros::Time::now();
+    odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_link";
+
+    // Position
+    odom.pose.pose.position.x = enu.e;
+    odom.pose.pose.position.y = enu.n;
+    odom.pose.pose.position.z = enu.u;
+
+    // Orientation (quaternion)
+    double theta = direction;  // Angle in radians
+    odom.pose.pose.orientation.x = 0.0;
+    odom.pose.pose.orientation.y = 0.0;
+    odom.pose.pose.orientation.z = sin(theta / 2);
+    odom.pose.pose.orientation.w = cos(theta / 2);
+
+    // Twist (not necessary for your case, but included for completeness)
+    odom.twist.twist.linear.x = 0.0;
+    odom.twist.twist.linear.y = 0.0;
+    odom.twist.twist.linear.z = 0.0;
+    odom.twist.twist.angular.x = 0.0;
+    odom.twist.twist.angular.y = 0.0;
+    odom.twist.twist.angular.z = 0.0;
+
+    // Publish the message 
+    odom_pub.publish(odom);
+
     prev_enu = enu;
 
 }
@@ -103,7 +140,10 @@ int main(int argc, char **argv){
         
         ros::init(argc, argv, "gps_to_odom");
         ros::NodeHandle n;
+
         ros::Subscriber gps_to_odom = n.subscribe("/fix", 10, Callback);
+        odom_pub = n.advertise<nav_msgs::Odometry>("gps_odom", 10);
+
         ros::spin();
 
         return 0;
